42dd6c99ecaf591effb11ba9dfac771b0ab44299
Andrey Klemenko <web.klemenko@gmail.com>
2021-04-22 22:12:59 +0300

    test3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 
 function App() {
   return (
-    sdfsdf
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
